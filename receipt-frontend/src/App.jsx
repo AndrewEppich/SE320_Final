664cc95a20926104e22a5397ab0b49380cae46fa
Graedon Beeler <115668636+gbeelerr@users.noreply.github.com>
2025-05-08 17:13:50 -0700

    added routing for other pages

--- receipt-frontend/src/App.jsx
+++ receipt-frontend/src/App.jsx
@@ -1,13 +1,23 @@
 import { Container, Row, Col, Card, Button, ListGroup, Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Header from "./Components/Header";
 import Home from "./Pages/Home";
+import Scan from "./Pages/Scan";
+import Summaries from "./Pages/Summaries";
+import History from "./Pages/History";
 function App() {
   return (
-    <>
+    <Router>
     <Header />
-    <Home />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Scan" element={<Scan />} />
+     <Route path="/Summaries" element={<Summaries />} />
+      <Route path="/History" element={<History />} />
+    </Routes>
+
+    </Router>
   );
 }
 
---
+++ receipt-frontend/src/Pages/Scan.jsx
@@ -0,0 +1,13 @@
+
+
+
+function Scan() {
+  return (
+    <div className="scan">
+      <h1>Scan</h1>
+      <p>Scan your receipt here.</p>
+    </div>
+  );
+}
+
+export default Scan;
